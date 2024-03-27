11fec6d8deedf32b30a3defc727147f9074cc0e4
Ilya_Solov <7156643@gmail.com>
2024-03-27 18:27:02 +0300

    Implemented bestsellers section

---
+++ js/swiper.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".bestsellerSwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    loop: false,
+    freeMode: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
