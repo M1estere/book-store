ac25aa16796b319bf67d36da5c3244c793e4b9a9
Ilya_Solov <7156643@gmail.com>
2024-05-25 22:28:22 +0300

    Current section bg color control

--- public/js/admin/section_switch.js
+++ public/js/admin/section_switch.js
@@ -1,7 +1,13 @@
 window.addEventListener('load', () => {
+    
+    $('#add-button').addClass('bg-gray-500');
 
     $('#add-button').on('click', function (e) {
         $('#status-text').addClass('hidden');
+
+        $('#add-button').addClass('bg-gray-500');
+        $('#edit-button').removeClass('bg-gray-500');
+        $('#delete-button').removeClass('bg-gray-500');
 
         $('#add-section').removeClass('hidden');
         $('#edit-section').addClass('hidden');
@@ -11,6 +17,10 @@ window.addEventListener('load', () => {
     $('#edit-button').on('click', function (e) {
         $('#status-text').addClass('hidden');
 
+        $('#add-button').removeClass('bg-gray-500');
+        $('#edit-button').addClass('bg-gray-500');
+        $('#delete-button').removeClass('bg-gray-500');
+
         $('#add-section').addClass('hidden');
         $('#edit-section').removeClass('hidden');
         $('#delete-section').addClass('hidden');
@@ -19,6 +29,10 @@ window.addEventListener('load', () => {
     $('#delete-button').on('click', function (e) {
         $('#status-text').addClass('hidden');
         
+        $('#add-button').removeClass('bg-gray-500');
+        $('#edit-button').removeClass('bg-gray-500');
+        $('#delete-button').addClass('bg-gray-500');
+
         $('#add-section').addClass('hidden');
         $('#edit-section').addClass('hidden');
         $('#delete-section').removeClass('hidden');
